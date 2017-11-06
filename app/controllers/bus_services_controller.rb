02eead96a6e6278c014a6a9e8d6c85ea3fe08490
Kenneth Lu <lu.yang.kenneth@gmail.com>
2017-11-06 13:51:47 +0800

    Render plaintext from BusStops and BusServices CSVs

--- app/controllers/bus_services_controller.rb
+++ app/controllers/bus_services_controller.rb
@@ -1,5 +1,12 @@
 class BusServicesController < ApplicationController
   def index
-    render plain: 'bus services'
+    render plain: bus_services_csv
+  end
+
+  private
+
+  def bus_services_csv
+    csv_path = "#{Rails.root}/lib/csvs/bus-services.csv"
+    File.read(csv_path)
   end
 end
--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,5 +1,12 @@
 class BusStopsController < ApplicationController
   def index
-    render plain: 'bus stops'
+    render plain: bus_stops_csv
+  end
+
+  private
+
+  def bus_stops_csv
+    csv_path = "#{Rails.root}/lib/csvs/bus-stops.csv"
+    File.read(csv_path)
   end
 end
