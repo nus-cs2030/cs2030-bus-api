c7301bed386ecf34de451a9ca7972bc69b6edd96
Kenneth Lu <lu.yang.kenneth@gmail.com>
2017-11-16 17:24:00 +0800

    Remove delay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def respond_with(text)
-    sleep(0.1)
     render plain: text
   end
 
