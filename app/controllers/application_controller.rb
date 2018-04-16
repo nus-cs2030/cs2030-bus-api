4d96aa6df1506ab74be883aeb014484f1847ea04
Kenneth Lu <lu.yang.kenneth@gmail.com>
2018-04-16 22:43:42 +0800

    Delay API responses unless a '?no_delay' URL flag is specified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def respond_with(text)
+    introduce_delay unless params.key?(:no_delay)
     render plain: text
   end
 
   private
+
+  def introduce_delay
+    puts 'Introducing 100ms delay...'
+    sleep(0.1)
+  end
 
   def record_not_found
     render plain: '404 Not Found', status: 404
